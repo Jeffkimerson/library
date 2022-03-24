c48e7efe3fadd8e0900f4f2957c88685ad24ff32
Jeffkimerson <85892155+Jeffkimerson@users.noreply.github.com>
2022-03-24 12:28:00 -0700

    Create variables for elements of modal

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ function addBookToLibrary() {
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+
+const modal = document.getElementById('bookModal');
+
+const modalCloseBtn = document.getElementById('modal-close-btn');
+
+//Create button event to open modal for user input
+const modalButton = document.getElementById('modal-btn');
+modalButton.addEventListener('click', () => {
+
+
+})
+
+const openModal = function() {
+    //Add CSS changes to open modal
+}
