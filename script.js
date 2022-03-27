f391a34f5c35a5f1205796dccec1461fd6512789
Jeff Kim <jeffersontaekim@gmail.com>
2022-03-26 19:16:44 -0700

    Add open and close modal functionality

--- script.js
+++ script.js
@@ -23,10 +23,18 @@ const modalCloseBtn = document.getElementById('modal-close-btn');
 //Create button event to open modal for user input
 const modalButton = document.getElementById('modal-btn');
 modalButton.addEventListener('click', () => {
-
+    modal.style.display = 'block';
 
 })
 
-const openModal = function() {
-    //Add CSS changes to open modal
+//Close modal with click on 'x' or outside modal
+modalCloseBtn.onclick = function() {
+    modal.style.display = 'none';
 }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+
